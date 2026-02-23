69c0699c6634c315186e7d292c7d83fcd5a73891
Erthryx <rom.maddox@gmail.com>
2026-02-23 15:27:24 +0100

    I added a temporal split for the val in the crossval_split.py that still needs to be tested and the 5stage_planner

--- nnunetv2/utilities/crossval_split.py
+++ nnunetv2/utilities/crossval_split.py
@@ -14,3 +14,30 @@ def generate_crossval_split(train_identifiers: List[str], seed=12345, n_splits=5
         splits[-1]['train'] = list(train_keys)
         splits[-1]['val'] = list(test_keys)
     return splits
+
+# HERE I MODIFIED THE SOURCE CODE
+#################################################
+# def generate_temporal_split(train_identifiers: List[str], n_splits: int = 5) -> List[dict[str, List[str]]]:
+#     """
+#     Temporal cross-validation split.
+    
+#     fold_0 -> last 20% as validation
+#     fold_1 -> previous 20%
+#     ...
+#     fold_4 -> first 20%
+#     Output format is identical to nnU-Net crossval split.
+#     """
+#     identifiers = np.array(train_identifiers)
+#     n_cases = len(identifiers)
+
+#     fold_size = n_cases // n_splits
+#     splits = []
+
+#     for fold in range(n_splits):
+#         val_start = n_cases - (fold + 1) * fold_size
+#         val_end   = n_cases - fold * fold_size if fold != 0 else n_cases
+#         val_idx = np.arange(val_start, val_end)
+#         train_idx = np.setdiff1d(np.arange(n_cases), val_idx)
+#         splits.append({"train": identifiers[train_idx].tolist(), "val": identifiers[val_idx].tolist()})
+
+#     return splits
